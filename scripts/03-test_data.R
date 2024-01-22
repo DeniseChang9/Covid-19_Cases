66e6763e0fbe22f59240805c86a192d6e4de2fc7
Denise Chang <ddleletan@hotmail.com>
2024-01-22 15:35:25 -0500

    test clean data

---
+++ scripts/03-test_data.R
@@ -0,0 +1,44 @@
+#### Preamble ####
+# Purpose: Tests the cleaned data of COVID-19 cases in Toronto
+# Author: Denise Chang
+# Date: 18 January 2024
+# Contact: dede.chang@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 01-download_data.R and 02-data_cleaning.R
+
+#### Workspace setup ####
+library(tidyverse)
+library(arrow)
+
+#### Test Data ####
+
+# read the cleaned data
+clean_covid_data <-
+  read_parquet(file = "outputs/data/clean_covid_data.parquet")
+
+head(clean_covid_data)
+
+# check there are only 2 hospitalization possibilities
+clean_covid_data$hospitalized |>
+  unique() |>
+  length() == 2
+
+# check the 2 hospitalization possibilities
+clean_covid_data$hospitalized |>
+  unique() == c("No", "Yes")
+
+# check there are only 2 icu possibilities
+clean_covid_data$icu |>
+  unique() |>
+  length() == 2
+
+# check the 2 icu possibilities
+clean_covid_data$icu |>
+  unique() == c("No", "Yes")
+
+# check the date range
+clean_covid_data$date |>
+  min() == "2020-01"
+
+clean_covid_data$date |>
+  max() == "2023-12"
